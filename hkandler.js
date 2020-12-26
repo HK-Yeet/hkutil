d345506f6c315451100d73d5adc60b18c328d3c6
HK-Yeet <>
2020-12-26 13:23:52 -0700

    Update hkandler.js

--- hkandler.js
+++ hkandler.js
@@ -1,69 +1,87 @@
 const {
-  Collection
+    Collection
 } = require("discord.js");
 const {
-  commandHandler,
-  eventHandler,
-  featureHandler
+    commandHandler,
+    eventHandler,
+    featureHandler
 } = require("hkutilities/functions/handlers");
 const {
-  join
+    join
 } = require("path")
 class HKandler {
-  _bot;
-  _commandsDir = "commands";
-  _eventsDir = "events";
-  _featuresDir = "features"
-  constructor(bot) {
-      this._bot = bot
-      if (!bot) {
-          throw new Error(
-              "HKandler ❯ No Discord.JS Client provided ❯ Need further assistance? Join the discord https://hk-yeet.github.io/discord"
-          );
-      }
-      if (bot.commands) {
-          throw new Error(
-              "HKandler ❯ Please remove the definition of <client>.commands ❯ Need further assistance? Join the discord https://hk-yeet.github.io/discord"
-          );
-      } else {
-          bot.commands = new Collection();
-      }
-  }
-  setEventsDir(eventsDir) {
-      const {
-          path
-      } = require.main;
-      const directory = join(path, eventsDir)
-      eventHandler(this._bot, directory)
-      this._eventsDir = eventsDir
-      return
-  }
-  getEventsDir() {
-      return this._eventsDir
-  }
-  setCommandsDir(commandsDir) {
-      const {
-          path
-      } = require.main;
-      const directory = join(path, eventsDir)
-      commandHandler(this._bot, directory)
-      this._commandsDir = commandsDir
-      return
-  }
-  getCommandsDir() {
-      return this._commandsDir
-  }
-  setFeaturesDir(featuresDir) {
-      const {
-          path
-      } = require.main;
-      const directory = join(path, featuresDir)
-      commandHandler(this._bot, directory)
-      this._featuresDir = featuresDir
-      return
-  }
-  getFeaturesDir() {
-      return this._featuresDir
-  }
+    _bot;
+    _commandsDir = "commands";
+    _eventsDir = "events";
+    _featuresDir = "features"
+    _setCmdPath = false
+    _setEvtPath = false
+    _setFeatPath = false
+    constructor(bot) {
+        this._bot = bot
+        if (!bot) {
+            throw new Error(
+                "HKandler ❯ No Discord.JS Client provided ❯ Need further assistance? Join the discord https://hk-yeet.github.io/discord"
+            );
+        }
+        if (bot.commands) {
+            throw new Error(
+                "HKandler ❯ Please remove the definition of <client>.commands ❯ Need further assistance? Join the discord https://hk-yeet.github.io/discord"
+            );
+        } else {
+            bot.commands = new Collection();
+        }
+        if (module && require.main) {
+            const {
+                path
+            } = require.main;
+            if (path) {
+                if(this._setEvtPath && this._setCmdPath && this._setFeatPath){
+                    commandHandler(bot, join(path, this.getCommandsDir()))
+                    eventHandler(bot, join(path, this.getEventsDir()))
+                    featureHandler(bot, join(path, this.getFeaturesDir()))
+                }
+            }
+        }
+    }
+    setEventsDir(eventsDir) {
+        this._setEvtPath = true
+        this._eventsDir = eventsDir
+        this.init()
+        return this
+    }
+    getEventsDir() {
+        return this._eventsDir
+    }
+    setCommandsDir(commandsDir) {
+        this._setCmdPath = true
+        this._commandsDir = commandsDir
+        this.init()
+        return this
+    }
+    getCommandsDir() {
+        return this._commandsDir
+    }
+    setFeaturesDir(featuresDir) {
+        this._setFeatPath = true
+        this._featuresDir = featuresDir
+        this.init()
+        return this
+    }
+    getFeaturesDir() {
+        return this._featuresDir
+    }
+    init(){
+        const {
+            path
+        } = require.main;
+        if (path) {
+        if(this._setEvtPath && this._setCmdPath && this._setFeatPath){
+            commandHandler(this._bot, join(path, this.getCommandsDir()))
+            eventHandler(this._bot, join(path, this.getEventsDir()))
+            featureHandler(this._bot, join(path, this.getFeaturesDir()))
+        }
+    }
+    }
 }
 module.exports = HKandler;
